53bccdb63b46ebf662c6a9c15db46e6be215af19
gujj <gujj@primeton.com>
2016-09-27 08:17:41 +0800

    update

--- shell/ocserv/install.sh
+++ shell/ocserv/install.sh
@@ -4,7 +4,7 @@ export OCSERV_VERSION=0.11.5
 
 yum install -y gnutls gnutls-utils libev perl-libintl libnl3 libseccomp pam lz4 readline \
                gcc gcc-c++ gnutls-devel libev-devel libnl3-devel libseccomp-devel \
-               kernel-headers pam-devel readline-devel tar xz \
+               kernel-headers pam-devel readline-devel tar xz
 
 curl -SL "ftp://ftp.infradead.org/pub/ocserv/ocserv-$OCSERV_VERSION.tar.xz" -o ocserv.tar.xz
 curl -SL "ftp://ftp.infradead.org/pub/ocserv/ocserv-$OCSERV_VERSION.tar.xz.sig" -o ocserv.tar.xz.sig
@@ -20,6 +20,29 @@ make install
 mkdir -p /etc/ocserv
 mkdir -p /etc/ocserv/config-per-group
 
+
+# Open ipv4 ip forward
+sysctl -w net.ipv4.ip_forward=1
+
+# Enable NAT forwarding
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+
+
+iptables -A FORWARD -s 192.168.99.0/24 -j ACCEPT
+
+iptables -t nat -A POSTROUTING -s 192.168.99.0/24 -o eth0 -j MASQUERADE
+
+iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+
+iptables -A FORWARD -o vpns0 -j ACCEPT
+
+iptables -A FORWARD -i vpns0 -j ACCEPT
+
+# Enable TUN device
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
 
 ln -s /usr/lib/systemd/system/ocserv.service /etc/systemd/system/multi-user.target.wants/
 
