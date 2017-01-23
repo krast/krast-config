c67e54b6324baa99448592b86d3a9dc95d61750c
krast <krast@live.cn>
2017-01-23 09:09:11 +0800

    test

--- shell/nghttp2/install.sh
+++ shell/nghttp2/install.sh
@@ -30,5 +30,5 @@ ln -s /usr/lib/systemd/system/nghttp2.service /etc/systemd/system/multi-user.tar
 
 firewall-cmd --zone=public --permanent --list-services
 firewall-cmd --zone=public --permanent --add-service=nghttp2
-firewall-cmd --zone=public --permanent --delete-service=nghttp2
+firewall-cmd --zone=public --permanent --remove-service=nghttp2
 firewall-cmd --reload
